b675959fd5ad1b46f7dcffc278726f4678b6d92c
Ramesh R Patil <50594877+Rameshrpatil@users.noreply.github.com>
2023-07-26 16:06:08 +0530

    Add files via upload

---
+++ program7/question7.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+#include<fcntl.h>
+
+int main(int argc,char* argv[])
+{
+	int fd=0;
+	int ret=0;
+	int arr[200];
+	if(argc!=2)
+	{
+		printf("Error: insufficent arguments");
+		return -1;
+	}
+
+	fd=open(argv[1],O_RDONLY);
+	
+	if (fd== -1)
+	{
+		printf("error: unable to open the  file \n");
+	}
+	else
+	{
+		printf("file gets opened with fd=%d\n",fd);
+	}
+
+	ret = read(fd,arr,5);
+	if(ret !=5)
+	{
+		printf("Unable to read contents\n");
+	}
+	printf("read contents are: %s",arr);
+
+		
+	return 0;
+} 
